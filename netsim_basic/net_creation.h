5bd5f933ff48d1905df3ed1d6e91d044015aef8f
David Mödinger <david.moedinger@uni-ulm.de>
2019-10-24 12:47:36 +0200

    remodel to network creation objects and added groupOfSizeK

--- netsim_basic/net_creation.h
+++ netsim_basic/net_creation.h
@@ -8,28 +8,62 @@
 #include <cstdint>
 #include <vector>
 #include <random>
+#include "net_nodes.h"
 
-/**
- * Example network creator scheme that ensures at least k connections are created for each node.
- * More connections are very likely for nodes earlier in the vector.
- */
-template <typename T, uint16_t k, uint32_t tag=0>
-void uniformlyAtLeastK(std::vector<T>& nodes){
-    auto maximum = k>nodes.size()-1?nodes.size()-1:k;
+template<typename Node>
+class abstractNetCreation {
+public:
+    virtual void operator()(std::vector<Node>& nodes) = 0;
+    virtual ~abstractNetCreation() = default;
+};
 
-    static std::minstd_rand gen(std::random_device{}());
-    static std::uniform_int_distribution<uint32_t> dist(0, nodes.size()-1);
+template<typename Node>
+class uniformlyAtLeastK : public abstractNetCreation<Node> {
+private:
+    uint32_t K, tag;
+public:
+    explicit uniformlyAtLeastK(uint32_t K, uint32_t tag = 0) : K(K), tag(tag) {}
 
-    std::cout << time(NULL) << ": Creating network ... " << std::flush;
+    void operator()(std::vector<Node>& nodes) override {
+        auto maximum = K>nodes.size()-1?nodes.size()-1:K;
 
-    for(auto& host: nodes) {
-        while(host.amountConnections(tag)<maximum) {
-            auto rnd = dist(gen);
-            host.addConnection(nodes[rnd],tag);
+        static std::minstd_rand gen(std::random_device{}());
+        static std::uniform_int_distribution<uint32_t> dist(0, nodes.size()-1);
+
+        for(auto& host: nodes) {
+            while(host.amountConnections(tag)<maximum) {
+                auto rnd = dist(gen);
+                host.addConnection(nodes[rnd],tag);
+            }
         }
     }
+};
 
-    std::cout << "done." << std::endl;
-}
+template<typename Node>
+class groupOfSizeK : abstractNetCreation<Node> {
+private:
+    uint32_t K, tag;
+public:
+    explicit groupOfSizeK(uint32_t K, uint32_t tag = 0) : K(K), tag(tag) {}
+
+    void operator()(std::vector<Node>& nodes) override {
+        auto maximum = K>nodes.size()-1?nodes.size()-1:K;
+
+        std::vector<uint32_t> permutation(nodes.size());
+        for(auto i = 0; i < nodes.size(); i+=1)
+            permutation[i] = i;
+        std::random_shuffle(permutation.begin(), permutation.end());
+
+        // group in blocks of size K, if size not divisible by K, last group has size % K members
+        for(auto i = 0; i<nodes.size(); i+=K) {
+            for(auto e = 0; e<K && i+e<nodes.size(); e+=1) {
+                for(auto add = e+1; add<K && i+add<nodes.size(); add+=1){
+                    nodes[permutation[i+e]].addConnection(nodes[permutation[i+add]],tag);
+                }
+            }
+        }
+
+    }
+};
 
 #endif //NETSIM2_NET_CREATION_H
